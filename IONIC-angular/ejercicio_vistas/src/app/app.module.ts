dc3cc043ec8d394145b13c3cba2a0219b78a492a
fran1916 <francisco.parra@uptc.edu.co>
2018-05-01 20:18:46 -0500

    ejercicios realizados

---
+++ IONIC-angular/ejercicio_vistas/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
